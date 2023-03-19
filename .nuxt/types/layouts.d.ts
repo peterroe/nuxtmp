621e52dfed72e38f8095e6a32548f073deee74d8
linshuheng <linshuheng@kuaishou.com>
2023-03-19 16:48:59 +0800

    remove: dist

--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = "default" | "home"
-declare module "/Users/linshuheng/vscode/i/editor/node_modules/.pnpm/nuxt@3.3.1_qcr72c723if2sxlfnbsa57v4m4/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
